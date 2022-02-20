acbdb7b5fc890fe6b22143ea1fed9d0595977440
AnandSundar <anandsundar96@gmail.com>
2022-02-20 12:55:59 -0600

    added fetch call to github.com to get users in UserResults.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-
+import UserResults from '../components/layout/users/UserResults'
 function Home() {
   return (
-    <div>
-        <h1 className='text-6xl'> Welcome</h1>
-        {process.env.REACT_APP_GITHUB_TOKEN}
-    </div>
+    <>
+        <UserResults />
+    </>
   )
 }
 
